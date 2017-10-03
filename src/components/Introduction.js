ae4bd830f0085fd42ecda5d0c4360b4c584f1777
Stacey Fernandes <stacey.fernandes@rea-group.com>
2017-10-03 13:21:58 +1100

    Add intro text

---
+++ src/components/Introduction.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Introduction extends React.Component {
+  render() {
+    return (
+      <p>
+      Dr. Stephanie Fernandes is dedicated to providing chiropractic care with a difference.
+      Using a technique only a select group of Chiropractors in Australia use, called ADVANCED BIO-STRUCTURAL CORRECTION ( ABC)TM. Since graduating in 2011, Dr. Fernandes worked and trained under the guidance of the Australasian Instructors of this technique for 18 months. Dr. Fernandes then
+      moved to Blackwater, Queensland where she provided her chiropractic services for the next two and a half years. Now, Stepahnie is practising at 3 locations in Melbournes east and south eastern suburbs of Boronia, Cranbourne east and Dandenong.
+      </p>
+    )
+  }
+}
+
+export default Introduction;
